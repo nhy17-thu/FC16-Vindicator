262ce0e1bfddee9fcf0baeeae2bc14547cdb54ed
Niu Haoyu <niuhaoyu17@gmail.com>
2019-04-25 17:34:30 +0800

    Update mydll.cpp

--- mydll.cpp
+++ mydll.cpp
@@ -16,23 +16,26 @@ bool towerFound = false;
 TowerInfo targetTower;
 
 double GetDistance(TPoint& p1, TPoint& p2);
+int GetDistance1(TPoint& p1, TPoint& p2);
 void MoveToTarget(Info& info, TSoldier& soldier, TPoint& tar);	//最简单的移动方式：横着走再竖着走，从soldier.position到tar
 void StartStrategy(Info& info);	//开局策略：生产轻骑兵并抢占最近的塔
 
 //以下是存储信息的全局变量
-int myid=0;
-int mytownum=1;
-int mysodnum=0;
+int myid = 0;
+int mytownum = 1;
+int mysodnum = 0;
 int mode = 0;
-struct mySoldier
+struct mySoldier 
 {
-	int player=0;
-	int id=0;
-	int type=0;
-	int blood=100;
-	bool defense=0;//是否是用来防守的重弓兵
+	int player = 0;
+	int id = 0;
+	int type = 0;
+	int blood = 100;
+	bool defense = 0;//是否是用来防守的重弓兵
 	TPoint lastpos;
-	int outdated;//信息过时几回合
+	//int outdated;//信息过时几回合
+	double range;
+
 };
 struct myTower
 {
@@ -40,52 +43,239 @@ struct myTower
 	int id = 0;
 	int level = 0;
 	int blood = 100;
+	TPoint pos;
 };
 struct enemyTarget
 {
 	bool type;//0 tower ;1 sodier
 	int id;
 	int danger;
-	int attackpirority;
+	int attackpriority;
+	TPoint pos;
 };
 vector<myTower> mytow;
-vector<myTower> alltow;
+vector<myTower> enemytow;
 vector<mySoldier> mysod;
-vector<mySoldier> allsod;
+vector<mySoldier> enemysod;
 vector<enemyTarget> target;
 
-int findtarget()
+int findtarget(Info& info)
 {
 
+	return 0;
 }
 
-int attack()
+int attack(Info& info)
 {
 
+	return 0;
 }
-int judgemode()
+int judgemode(Info& info)
 {
 	//not ready;建造重弓兵，并移动至合适的位置
 	//safe;没有战事，但自己也没有多余兵力进攻
 	//attack;准备进攻敌方塔
 	//underattack;自己的塔被攻击/正在和敌人交火
+	return 0;
 }
-int updateinfo(Info& info)
-{
+int updateinfo(Info& info) {
+	mytow.clear();
+	enemytow.clear();
+	mysod.clear();
+	enemysod.clear();
+	//target.clear();
 	myid = info.myID;
-	//mysodnum
+	mytownum = 0;
+	mysodnum = 0;
+	//mode = 0?
+	//towers
+	for (int i = 0; i < info.towerInfo.size(); i++) {
+		myTower temp;
+		if (info.towerInfo[i].owner == info.myID) {
+			mytownum++;
+			temp.player = info.myID;
+			temp.id = info.towerInfo[i].id;
+			temp.level = info.towerInfo[i].level;
+			temp.blood = info.towerInfo[i].blood;
+			temp.pos = info.towerInfo[i].position;
+			mytow.push_back(temp);
+		}
+		else {
+			temp.player = info.towerInfo[i].owner;
+			temp.id = info.towerInfo[i].id;
+			temp.level = info.towerInfo[i].level;
+			temp.blood = info.towerInfo[i].blood;
+			temp.pos = info.towerInfo[i].position;
+			enemytow.push_back(temp);
+		}
+	}
+	//soldiers
+	for (int i = 0; i < info.soldierInfo.size(); i++) {
+		mySoldier temp;
+		if (info.soldierInfo[i].owner == info.myID) {
+			mysodnum++;
+			temp.player = info.myID;
+			temp.id = info.soldierInfo[i].id;
+			temp.type = info.soldierInfo[i].type;
+			temp.blood = info.soldierInfo[i].blood;
+			//假设重工兵都是用来防守的
+			temp.defense = (info.soldierInfo[i].type == HeavyArcher ? 1 : 0);
+			temp.lastpos = info.soldierInfo[i].position;
+			temp.range = info.soldierInfo[i].range;
+			mysod.push_back(temp);
+		}
+		else {
+			temp.player = info.soldierInfo[i].owner;
+			temp.id = info.soldierInfo[i].id;
+			temp.type = info.soldierInfo[i].type;
+			temp.blood = info.soldierInfo[i].blood;
+			temp.defense = 0;
+			temp.lastpos = info.soldierInfo[i].position;
+			temp.range = info.soldierInfo[i].range;
+			enemysod.push_back(temp);
+		}
+	}
+}
+int unitattack(Info& info,mySoldier attacker,enemyTarget target)
+{
+	bool inplace = 0;
+	int x_pos = 0;
+	int y_pos = 0;
+	if (target.type == 1)
+	{
+		double dis = GetDistance(attacker.lastpos, target.pos);
+		if (dis < attacker.range) 
+		{ 
+			inplace = 1; 
+			x_pos = target.pos.x;
+			y_pos = target.pos.y;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 1; i++)
+		{
+			double dis = GetDistance(attacker.lastpos, target.pos);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target.pos.x;
+				y_pos = target.pos.y;
+				break;
+			}
+			TPoint target1 = target.pos;
+			target1.x -= 1;
+			dis = GetDistance(attacker.lastpos, target1);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target1.x;
+				y_pos = target1.y;
+				break;
+			}
+			TPoint target2 = target.pos;
+			target2.x -= 1;
+			target2.y += 1;
+			dis = GetDistance(attacker.lastpos, target2);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target2.x;
+				y_pos = target2.y;
+				break;
+			}
+			TPoint target3 = target.pos;
+			target3.y += 1;
+			dis = GetDistance(attacker.lastpos, target3);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target3.x;
+				y_pos = target3.y;
+				break;
+			}
+			TPoint target4 = target.pos;
+			target4.x += 1;
+			target4.y += 1;
+			dis = GetDistance(attacker.lastpos, target4);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target4.x;
+				y_pos = target4.y;
+				break;
+			}
+			TPoint target5 = target.pos;
+			target5.x += 1;
+			dis = GetDistance(attacker.lastpos, target5);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target5.x;
+				y_pos = target5.y;
+				break;
+			}
+			TPoint target6 = target.pos;
+			target6.x += 1;
+			target6.y -= 1;
+			dis = GetDistance(attacker.lastpos, target6);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target6.x;
+				y_pos = target6.y;
+				break;
+			}
+			TPoint target7 = target.pos;
+			target7.y -= 1;
+			dis = GetDistance(attacker.lastpos, target7);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target7.x;
+				y_pos = target7.y;
+				break;
+			}
+			TPoint target8 = target.pos;
+			target8.x -= 1;
+			target8.y -= 1;
+			dis = GetDistance(attacker.lastpos, target8);
+			if (dis < attacker.range)
+			{
+				inplace = 1;
+				x_pos = target8.x;
+				y_pos = target8.y;
+				break;
+			}
+		}
+	}
+	if (inplace == 1)
+	{
+		info.myCommandList.addCommand(Attack,attacker.id, x_pos, y_pos);
+	}
+	else
+	{
+		TPoint movetarget;
+		if (target.type = 1)
+		{
+			////////////////////////////////////
+		}
+	}
 }
 
 void player_ai(Info& info)
 {
 	if (!startStageFinished) {
 		StartStrategy(info);
-	} else {
-		updateinfo(info);
-		mode = judgemode();
-		findtarget();
-		attack();
+		return;
 	}
+	{
+		updateinfo(info);
+		mode = judgemode(info);
+		findtarget(info);
+		attack(info);
+	}
+	cout << info.round << endl;
 }
 
 
@@ -94,6 +284,9 @@ double GetDistance(TPoint& p1, TPoint& p2) {
 	return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 }
 
+int GetDistance1(TPoint& p1, TPoint& p2) {
+	return (abs(p1.x - p2.x) + abs(p1.y - p2.y));
+}
 
 
 //最简单的移动方式：横着走再竖着走，从soldier.position到tar
@@ -130,7 +323,10 @@ void StartStrategy(Info& info) {
 		}
 	}
 
-	if (info.round > 5) {	//轻骑兵已生产
+	if (info.round > 5) 
+	{
+		startStageFinished = 1;
+		//轻骑兵已生产
 		if (towerFound && targetTower.owner == info.myID) {
 			startStageFinished = true;
 			return;
@@ -155,15 +351,15 @@ void StartStrategy(Info& info) {
 			}
 		}
 		//移动到指定地点并攻击
-		if (GetDistance(LK.position, targetTower.position) > 2.5) { //sqrt5
+		/*if (GetDistance(LK.position, targetTower.position) > 2.5) { //sqrt5
 			MoveToTarget(info, LK, targetTower.position);
 		}
 		else {
 			for (int dx = -1; dx <= 1; ++dx) {
 				for (int dy = -1; dy <= 1; ++dy) {
-					info.myCommandList.addCommand(Attack, LK.id, LK.x_position+dx, LK.y_position+dy);
+					info.myCommandList.addCommand(Attack, LK.id, LK.x_position + dx, LK.y_position + dy);
 				}
 			}
-		}
+		}*/
 	}
 }
